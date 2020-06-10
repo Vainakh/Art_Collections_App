e189726d9a651515661d501ac4167a45a191cf43
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-06-09 17:31:14 -0700

    Fix sessions controller and fix LogIn ejs

--- controllers/sessions_controller.js
+++ controllers/sessions_controller.js
@@ -4,7 +4,7 @@ const sessions = express.Router()
 const User = require('../models/users.js')
 
 sessions.get('/new', (req, res) => {
-  res.render('login.ejs')
+  res.render('logIn.ejs',  {currentUser: req.session.currentUser} )
 })
 
 // on sessions form submit (log in)
